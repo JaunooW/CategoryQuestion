43c814f7f66b2e17d3b79c37cd781e2aa29c77c3
P129BAD <P129BAD@L-PW02X08W>
2022-10-06 09:16:29 +0400

    Add project files.

---
+++ CategoryQuestion/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategoryQuestion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var category = new Class1();
+        }
+    }
+}
---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            //Displays Small Menu
+            Display();
+            //Values Entry
+            EntryValues();
+        }
+
+        public static void EntryValues()
+        {
+            var stop = -1;//Stopping Condition
+            do
+            {
+                //Entry of Values
+                Console.Write("Input Integer: ");
+                int point = int.Parse(Console.ReadLine());
+                if (point == stop)
+                {
+                    return;
+                }
+                //Compare Value and Displays the Category
+                CheckCategory(point);
+            } while (true);
+        }
+        public static void Display()
+        {
+            int previousValue = 0;
+            foreach (int i in Enum.GetValues(typeof(CategoryList.Category)))//Get each Integer of the enums
+            {
+                Console.WriteLine($"Category: {Enum.GetName(typeof(CategoryList.Category), i)} has a range of {previousValue} - {i}");
+                previousValue = i + 1;
+            }
+        }
+
+        public static void CheckCategory(int point)
+        {
+            foreach (int i in Enum.GetValues(typeof(CategoryList.Category)))
+            {
+                if (point <= i)
+                {
+                    Console.WriteLine($"{Enum.GetName(typeof(CategoryList.Category), i)}");
+                    return;
+                }
+            }
+        }
+    }
+    public class CategoryList
+    {
+        public enum Category
+        {
+            F = 10,
+            E = 20,
+            D = 40,
+            C = 60,
+            B = 80,
+            A = 100
+        }
+    }
+}
